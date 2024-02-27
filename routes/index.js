d89bf75246980c734043f9f8fdbd96f7137f7b49
Santiago Villabona <santienrike16anime@gmail.com>
2024-02-27 17:54:18 -0500

    organizacion

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,9 @@ const db = require('../db/database')
 
 var router = express.Router();
 
+//----------------------------------------------------------------------//
+
+// Llamar productos
 router.get('/', authOnly, async function (req, res, next) {
   let conn;
   try {
@@ -12,7 +15,7 @@ router.get('/', authOnly, async function (req, res, next) {
     conn = await db.pool.getConnection();
     const rows = await conn.query("SELECT * FROM producto");
     console.log(rows);
-    res.render('index.ejs', { title: 'Dashboard', user: req.session.user, products: rows }); //[ {val: 1}, meta: ... ]
+    res.render('index.ejs', { title: 'Dashboard', user: req.session.user, products: rows });
 
   } catch (err) {
     console.log('Entre un error', err);
@@ -22,12 +25,19 @@ router.get('/', authOnly, async function (req, res, next) {
     }
 });
 
+//-----------------------------------------------------------//
+
+
+// Llamar usuario
 router.get('/auth', function (req, res, next) {
   req.session.regenerate(function () {
     req.session.user = {'name': 'santiago', 'id': 16};
     res.redirect('/');
   });
 });
+
+//----------------------------------------------------------//
+
 
 router.get('/productos/eliminar/:productoid', async function (req, res, next) {
   let conn;
@@ -46,7 +56,9 @@ router.get('/productos/eliminar/:productoid', async function (req, res, next) {
     }
 });
 
-//Funcion de llamar cartas
+//--------------------------------------------//
+
+// Funcion de llamar cartas
 router.get('/lista_deseos', authOnly, async function (req, res, next) {
   let conn;
   try {
@@ -64,7 +76,9 @@ router.get('/lista_deseos', authOnly, async function (req, res, next) {
     }
 });
 
-//Funcion de eliminar carta
+//------------------------------------------------------------------//
+
+// Funcion de eliminar carta
 router.get('/lista_deseos/eliminar/:itemid', async function (req, res, next) {
   let conn;
   try {
